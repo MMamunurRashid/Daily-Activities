4226fd22e537394adc94b1b41262b65a5351d72b
Mamunur-Rashid <mdmamun.iubat.m@gmail.com>
2022-09-29 02:56:03 +0600

    hard coded user done

--- src/Component/User/User.js
+++ src/Component/User/User.js
@@ -16,8 +16,50 @@ const User = () => {
           Web Developer
         </h1>
         <h1 className=" text-lg font-semibold text-center">Student</h1>
+        <div className="mt-10">
+          <h1 className="pl-3 text-lg font-semibold">Add a Break</h1>
+          <div className="text-center mt-3 rounded bg-gray-300 py-5 mx-2">
+            <a
+              className="text-white font-semibold p-3 rounded-full ml-1 bg-red-400 hover:bg-indigo-700"
+              href="/"
+            >
+              10m
+            </a>
+            <a
+              className="text-white font-semibold p-3 rounded-full ml-1 bg-red-400 hover:bg-indigo-700"
+              href="/"
+            >
+              15m
+            </a>
+            <a
+              className="text-white font-semibold p-3 rounded-full ml-1 bg-red-400 hover:bg-indigo-700"
+              href="/"
+            >
+              20m
+            </a>
+            <a
+              className="text-white font-semibold p-3 rounded-full ml-1 bg-red-400 hover:bg-indigo-700 "
+              href="/"
+            >
+              30m
+            </a>
+          </div>
+        </div>
+        <div>
+          <h1 className="mt-14 ml-3 text-lg font-semibold">
+            Activities Details
+          </h1>
+          <div className="rounded bg-gray-200 p-3 m-3">
+            <h1 className="font-semibold ml-">Activities Time: {} minute</h1>
+          </div>
+          <div className="rounded bg-gray-200 p-3 m-3">
+            <h1 className="font-semibold ml-">Break Time: {} minute</h1>
+          </div>
+        </div>
         <div className="">
-          <h1 className="pl-5 text-lg font-semibold">Add a Break</h1>
+          <button className="mt-14 text-lg text-white ml-12 font-bold p-3 rounded-lg bg-indigo-700 hover:bg-gray-400">
+            Activity Completed
+          </button>
         </div>
       </div>
     </div>
